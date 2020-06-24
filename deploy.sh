748cc7a4cab860b8b15f2d32ad24a18a4acd11e5
s.mikulich <serjman91@gmail.com>
2020-06-24 22:41:38 +0300

    Test build on push

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+echo $(aws ecr get-authorization-token --region eu-central-1 --output text --query 'authorizationData[].authorizationToken' | base64 -d | cut -d: -f2) | docker login -u AWS https://083365510130.dkr.ecr.eu-central-1.amazonaws.com --password-stdin
+docker pull 083365510130.dkr.ecr.eu-central-1.amazonaws.com/simple-node-app:latest
+docker run -d -p 80:5000 083365510130.dkr.ecr.eu-central-1.amazonaws.com/simple-node-app:latest
+
+#docker-compose up
--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Simplest Node app builded and deployed</h1>`);
+    res.send(`
+        <h1>Simplest Node app builded and deployed</h1>
+        <h1>Build and deploy on push</h1>
+    `);
 });
 
 app.listen(5000, function () {
