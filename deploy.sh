cd558860de7c6aae7e852202483b1fd0347a43a9
Sergey Mikulich <s.mikulich@syberry.com>
2020-06-25 12:31:35 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+docker stop `docker ps | grep "/bin/sh" | awk '{print $1}'`
+docker rm `docker ps | grep "/bin/sh" | awk '{print $1}'`
 echo $(aws ecr get-authorization-token --region eu-central-1 --output text --query 'authorizationData[].authorizationToken' | base64 -d | cut -d: -f2) | docker login -u AWS https://083365510130.dkr.ecr.eu-central-1.amazonaws.com --password-stdin
 docker pull 083365510130.dkr.ecr.eu-central-1.amazonaws.com/simple-node-app:latest
 docker run -d -p 80:5000 083365510130.dkr.ecr.eu-central-1.amazonaws.com/simple-node-app:latest
